ccd4196c543a84c3dfea42d07b1da51798830ae1
Dave Milici <milici.dave@gmail.com>
2024-05-12 13:32:35 -0700

    Modify build.sh script with MacOSX vs Linux options.

--- build-dxx.sh
+++ build-dxx.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
-#export CXX=g++-7
-#export CXXFLAGS=-Wno-uninitialized
-export CXX=g++-10
-export CXXFLAGS=-DDXX_USE_STEREOSCOPIC_RENDER=1
-scons -j4 d1x=1 d2x=1 use_stereo_render=1
+
+# Mac OS X or Linux?
+UNAME=`uname`
+if [ $UNAME == "Darwin" ]
+then
+	CPUS=`sysctl -n hw.logicalcpu`
+	JOBS="-j$((CPUS))"
+	OPTS="macos_add_frameworks=false sdl2=true sconf_cxx20=1 use_stereo_render=1"
+	export CXX=g++
+	export CXXFLAGS=-Wno-uninitialized
+else
+	CPUS=`nproc`
+	JOBS="-j$((CPUS))"
+	OPTS="use_stereo_render=1"
+	export CXX=g++-10
+	export CXXFLAGS=-DDXX_USE_STEREOSCOPIC_RENDER=1
+fi
+
+echo "kernel=$UNAME, cpus=$CPUS, scons=$OPTS"
+scons $JOBS $OPTS
